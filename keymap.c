4690c7900417eff183865fae832aef3b6704afb4
Andrey Vityuk <andrey.vityuk@gmail.com>
2022-06-21 18:34:20 -0700

    Implemented MacOS Home/End macros

--- keymap.c
+++ keymap.c
@@ -3,6 +3,11 @@
 #define QWERTY 0 // Base qwerty
 
 
+enum custom_keycodes {
+    MY_HOME = SAFE_RANGE,
+		MY_END,
+};
+
 /****************************************************************************************************
 *
 * Keymap: Default Layer in Qwerty
@@ -35,12 +40,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_NO,    QK_BOOT,
     KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    KC_RCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
               KC_GRV,   KC_INS,   KC_LEFT,  KC_RGHT,                                                                                       KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
                                                       KC_LCMD,  KC_LALT,                                               KC_RCTL,  KC_RCMD,
-                                                                KC_HOME,                                               KC_PGUP,
-                                            KC_BSPC,  KC_DEL,   KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
+                                                                MY_HOME,                                               KC_PGUP,
+                                            KC_BSPC,  KC_DEL,   MY_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
   )
 
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case MY_HOME:
+        if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LCMD) SS_DELAY(50) SS_TAP(X_LEFT) SS_UP(X_LCMD));
+        } else {
+            // when keycode MY_HOME is released
+        }
+        break;
+    case MY_END:
+        if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LCMD) SS_DELAY(50) SS_TAP(X_RIGHT) SS_UP(X_LCMD));
+        } else {
+            // when keycode MY_END is released
+        }
+        break;
+    }
+    return true;
+};
+
