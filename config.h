ddd63948cce9ef9d86753f24769fd232f881f868
Andrey Vityuk <andrey.vityuk@gmail.com>
2022-06-22 23:21:37 -0700

    Fixed Space Cadet Shift and Caps Word on both Shifts

---
+++ config.h
@@ -0,0 +1,5 @@
+// Activate Command with Left Cmd + Right Cmd.
+#define IS_COMMAND() (get_mods() == MOD_MASK_GUI)
+
+// Activate Caps Word on Left Shift + Right Shift.
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
