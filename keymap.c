03c94453d25f1c0253d27d38efe645e88a581ad2
Andrey Vityuk <andrey.vityuk@gmail.com>
2022-06-22 22:49:54 -0700

    Remap Command mode to boths Cmd and enable Caps Word on both Shifts

--- keymap.c
+++ keymap.c
@@ -1,5 +1,11 @@
 #include QMK_KEYBOARD_H
 
+// Activate Command with Left Cmd + Right Cmd.
+#define IS_COMMAND() (get_mods() == MOD_MASK_GUI)
+
+// Activate Caps Word on Left Shift + Right Shift.
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
 #define QWERTY 0 // Base qwerty
 
 // MacOS Home and End (Modify iTerm Key: LCMD + LEFT -> Hex Code 001 and LCMD + RIGHT -> Hex Code 005)
@@ -7,8 +13,8 @@
 #define MAC_END  LCMD(KC_RGHT)
 
 // Mod-tap keys for Left/Right Shift
-#define TH_LSFT LSFT_T(KC_LPRN)  // LSFT (tap) -> KC_LPRN; LSFT (held) -> KC_LSFT; 
-#define TH_RSFT RSFT_T(KC_RPRN)  // RSFT (tap) -> KC_RPRN; RSFT (held) -> KC_RSFT; 
+#define TH_LSFT LSFT_T(KC_LPRN)  // LSFT (tap) -> KC_LPRN; LSFT (held) -> KC_LSFT;
+#define TH_RSFT RSFT_T(KC_RPRN)  // RSFT (tap) -> KC_RPRN; RSFT (held) -> KC_RSFT;
 
 /****************************************************************************************************
 *
@@ -21,7 +27,7 @@
 * |--------+------+------+------+------+------|                           +------+------+------+------+------+--------|
 * | Tab    |   Q  |   W  |   E  |   R  |   T  |                           |   Y  |   U  |   I  |   O  |   P  | \|     |
 * |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
-* | Caps   |   A  |   S  |   D  |   F  |   G  |                           |   H  |   J  |   K  |   L  |  ;:  | '"     |
+* |        |   A  |   S  |   D  |   F  |   G  |                           |   H  |   J  |   K  |   L  |  ;:  | '"     |
 * |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
 * | Shift( |   Z  |   X  |   C  |   V  |   B  |                           |   N  |   M  |  ,.  |  .>  |  /?  | Shift) |
 * `--------+------+------+------+------+-------                           `------+------+------+------+------+--------'
@@ -42,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_NO,    QK_BOOT,
     KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
-    CAPSWRD,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    _______,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     TH_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  TH_RSFT,
               KC_GRV,   KC_INS,   KC_LEFT,  KC_RGHT,                                                                                       KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
                                                       KC_LCMD,  KC_LALT,                                               KC_RCTL,  KC_RCMD,
