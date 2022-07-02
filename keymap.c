0b52805406b48dab12d0e3be8d9d85acfc12f13c
Andrey Vityuk <andrey.vityuk@gmail.com>
2022-07-02 00:02:22 -0700

    Change 'progm' key from BOOT to COMMAND

--- keymap.c
+++ keymap.c
@@ -16,12 +16,16 @@
 #define KC_LCBO LCMD_T(KC_LCBR)  //  LCMD on hold, KC_LCBR on tap
 #define KC_RCBC RCMD_T(KC_RCBR)  //  RCMD on hold, KC_RCBR on tap
 
+enum custom_keycodes {
+  COMMAND = SAFE_RANGE,  // Command keycode.
+};
+
 /****************************************************************************************************
 *
 * Keymap: Default Layer in Qwerty
 *
 * ,-------------------------------------------------------------------------------------------------------------------.
-* | Esc    |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F8  |  F9  |  F10 |  F12 | PSCR | SLCK | PAUS |  FN0 |  BOOT  |
+* | Esc    |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F8  |  F9  |  F10 |  F12 | PSCR | SLCK | PAUS |  FN0 | COMMAND|
 * |--------+------+------+------+------+------+---------------------------+------+------+------+------+------+--------|
 * | =+     |  1!  |  2@  |  3#  |  4$  |  5%  |                           |  6^  |  7&  |  8*  |  9(  |  0)  | -_     |
 * |--------+------+------+------+------+------|                           +------+------+------+------+------+--------|
@@ -45,7 +49,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [QWERTY] = LAYOUT_pretty(
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_NO,    QK_BOOT,
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_NO,    COMMAND,
     KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
     CAPSWRD,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
@@ -85,6 +89,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     //     return false;
     //   }
     //   break;
+    case COMMAND:
+      if (record->event.pressed) {
+        register_code(KC_LCMD);
+        register_code(KC_RCMD);
+      } else {
+        unregister_code(KC_LCMD);
+        unregister_code(KC_RCMD);
+      }
 	}
 
 	return true;  // Return true to allow further processing of key.
