24b854cdb6b62abb31baaa830c95d6a83a5aee60
Andrey Vityuk <andrey.vityuk@gmail.com>
2022-07-11 14:28:41 -0700

    Fix right Shift mapping

--- keymap.c
+++ keymap.c
@@ -65,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
     TG(NAV),  KC_A,     KC_CMDS,  KC_D,     KC_CMDF,  KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    KC_LSFT,  KC_ZCTL,  KC_CMDX,  KC_CMDC,  KC_CMDV,  KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLCTL, KC_RSPC,
-              KC_GRV,   KC_INS,   KC_LEFT,  KC_RGHT,                                                                                       KC_UP,    KC_DOWN,  KC_LBRC,  KC_RSFT,
+    KC_LSFT,  KC_ZCTL,  KC_CMDX,  KC_CMDC,  KC_CMDV,  KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLCTL, KC_RSFT,
+              KC_GRV,   KC_INS,   KC_LEFT,  KC_RGHT,                                                                                       KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
                                                       KC_LCMD,  KC_LALT,                                               KC_RCTL,  KC_RCMD,
                                                                 MAC_HOME,                                              KC_PGUP,
                                             KC_BSPC,  KC_DEL,   MAC_END,                                               KC_PGDN,  KC_ENTER, KC_SPC
